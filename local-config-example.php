fcfc2522bfd9792c4604159dcc72ae55f9cc57a2
Daniel Cantarín <omega_canta@yahoo.com>
2013-11-07 14:45:57 -0300

    Changed the default user and database values, according to the last two commits.

--- local-config-example.php
+++ local-config-example.php
@@ -7,9 +7,9 @@
 $dbms = 'mysql'; // Probably mysql
 $dbhost = 'localhost'; // Probably localhost
 $dbport = ''; // Empty for default port
-$dbname = 'forofyl'; // Your DB name
-$dbuser = 'root'; // Probably root
-$dbpasswd = ''; // Probably left blank
+$dbname = 'forofyl_phpbb'; // Your DB name
+$dbuser = 'forofyl_admin'; // Change the default user for production!
+$dbpasswd = 'forofyl'; // Also change the pass for production!
 
 // ====================
 // Cache configuration.
