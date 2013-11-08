cdd5b54d74c228158084c7413224c8e48575e5a2
andrezrv <andrezrv@gmail.com>
2013-11-08 05:01:40 +0000

    Modified .sql scripts to use variables and dynamic SQL.

--- db/00-pre_database_settings.sql
+++ db/00-pre_database_settings.sql
@@ -1,7 +1,16 @@
+/**
+ * This file is intended to run specific tasks BEFORE main database creation.
+ */
+
+-- Adjust to the values you need --
+SELECT @db_user := 'forofyl_admin', @db_password := 'forofyl';
+
 /* Creating the developers user */
-CREATE USER 'forofyl_admin'@'localhost' IDENTIFIED BY 'forofyl';
+SET @create_for_local = CONCAT( 'CREATE USER "' , @db_user , '"@"localhost" IDENTIFIED BY "' , @db_password , '" ');
+PREPARE stmt FROM @create_for_local; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
 /* It's also created for remote access */
-CREATE USER 'forofyl_admin'@'%' IDENTIFIED BY 'forofyl';
+SET @create_for_remote = CONCAT( 'CREATE USER "' , @db_user , '"@"%" IDENTIFIED BY "' , @db_password , '" ');
+PREPARE stmt FROM @create_for_remote; EXECUTE stmt; DEALLOCATE PREPARE stmt;
 
 -- Don't forget to run 01-forofyl_phpbb.sql now, and then 02-post_database_settings.sql! --
