284f6a663dcdb2e71a83aa05a63f744a716c593c
andrezrv <andrezrv@gmail.com>
2013-11-08 03:49:04 +0000

    Updated comment about the execution order of MySQL scripts.

--- db/00-pre_database_settings.sql
+++ db/00-pre_database_settings.sql
@@ -4,4 +4,4 @@ CREATE USER 'forofyl_admin'@'localhost' IDENTIFIED BY 'forofyl';
 /* It's also created for remote access */
 CREATE USER 'forofyl_admin'@'%' IDENTIFIED BY 'forofyl';
 
--- Don't forget to run 02-post_database_settings.sql now! --
+-- Don't forget to run 01-forofyl_phpbb.sql now, and then 02-post_database_settings.sql! --
